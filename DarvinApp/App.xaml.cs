2dcf7876dfde722d07cda362ecde491d7ee8fb6f
Nina Buchina <ninn-ok@mail.ru>
2013-02-14 18:10:36 +0400

    Форма для вопросника
    Форма и модель для неё. Форма отображает текущий вопрос.

--- DarvinApp/App.xaml.cs
+++ DarvinApp/App.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using System.Windows;
+using DarvinApp.Business.DataTypes;
+using DarvinApp.Presentation;
 
 namespace DarvinApp
 {
@@ -7,5 +9,12 @@ namespace DarvinApp
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            var window = new MainWindow{ DataContext = new ViewModel{CurrentQuestion = new Question{Text = "Nyananananana"}} };
+            window.Show();
+        }
     }
 }
---
+++ DarvinApp/Presentation/ViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using DarvinApp.Business.DataTypes;
+
+namespace DarvinApp.Presentation
+{
+    public class ViewModel
+    {
+        public Question CurrentQuestion { get; set; }
+        public IList<Question> AvailableQuestions { get; set; }
+        private int _questionListIndex;
+
+        public ViewModel()
+        {
+            _questionListIndex = 0;
+        }
+
+        private void selectNextQuestion()
+        {
+            CurrentQuestion = AvailableQuestions[_questionListIndex++];
+        }
+    }
+}
