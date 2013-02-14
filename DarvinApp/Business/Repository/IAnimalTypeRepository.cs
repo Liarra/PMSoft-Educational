c2af13def05ace691ccc1b484df92bfae163379f
Nina Buchina <ninn-ok@mail.ru>
2013-02-14 17:09:46 +0400

    Добавлен недостающий метод для IAnimalTypeRepository

--- DarvinApp/Business/Repository/IAnimalTypeRepository.cs
+++ DarvinApp/Business/Repository/IAnimalTypeRepository.cs
@@ -1,7 +1,10 @@
-﻿namespace DarvinApp.Business.Repository
+﻿using System.Collections.Generic;
+using DarvinApp.Business.DataTypes;
+
+namespace DarvinApp.Business.Repository
 {
     public interface IAnimalTypeRepository
     {
-         
+        private IList<AnimalType> AnimalTypeList();
     }
 }
--- DarvinApp/DataAccess/Hardcode/HardcodeAnimalTypeRepository.cs
+++ DarvinApp/DataAccess/Hardcode/HardcodeAnimalTypeRepository.cs
@@ -1,9 +1,14 @@
-﻿using DarvinApp.Business.Repository;
+﻿using System.Collections.Generic;
+using DarvinApp.Business.DataTypes;
+using DarvinApp.Business.Repository;
 
 namespace DarvinApp.DataAccess.Hardcode
 {
     public class HardcodeAnimalTypeRepository:IAnimalTypeRepository
     {
-         
+        public IList<AnimalType> AnimalTypeList()
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
