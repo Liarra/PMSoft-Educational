5c756228efeb9f4165c305e89126d407d8d6fd86
Nina Buchina <ninn-ok@mail.ru>
2013-02-15 12:47:44 +0400

    Кнопки на форме
    Имена и привязка команд из ViewModel

--- DarvinApp/Presentation/ViewModel.cs
+++ DarvinApp/Presentation/ViewModel.cs
@@ -1,22 +1,39 @@
 ﻿using System.Collections.Generic;
+using System.Windows.Input;
 using DarvinApp.Business.DataTypes;
+using DarvinApp.Business.Repository;
+using GalaSoft.MvvmLight.Command;
 
 namespace DarvinApp.Presentation
 {
     public class ViewModel
     {
         public Question CurrentQuestion { get; set; }
-        public IList<Question> AvailableQuestions { get; set; }
         private int _questionListIndex;
+        private IList<Question> AvailableQuestions { get; set; }
+        private ICommand _yesButtonPushed, _noButtonPushed;
 
-        public ViewModel()
+        public ViewModel(IQuestionRepository questionsSource)
         {
             _questionListIndex = 0;
+            AvailableQuestions = questionsSource.GetAllQuestions();
         }
 
-        private void selectNextQuestion()
+        public ICommand YesButtonPushed
         {
+            get { return _yesButtonPushed ?? (_yesButtonPushed = new RelayCommand(SelectNextQuestion)); }
+        }
+
+        public ICommand NoButtonPushed
+        {
+            get { return _noButtonPushed ?? (_noButtonPushed = new RelayCommand(SelectNextQuestion)); }
+        }
+
+        private void SelectNextQuestion()
+        {
+            if(AvailableQuestions.Count==0)return;
             CurrentQuestion = AvailableQuestions[_questionListIndex++];
+            _questionListIndex = _questionListIndex%AvailableQuestions.Count;
         }
     }
 }
