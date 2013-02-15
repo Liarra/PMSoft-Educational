1888f76e1c2e63d260a7dc18ecff89e4e7f2f098
Nina Buchina <ninn-ok@mail.ru>
2013-02-15 17:23:47 +0400

    Ещё юнит-тесты эксперта

--- DarvinAppTest/Business/ExpertFixture.cs
+++ DarvinAppTest/Business/ExpertFixture.cs
@@ -1,4 +1,5 @@
-﻿using DarvinApp.Business;
+﻿using System.Collections.Generic;
+using DarvinApp.Business;
 using DarvinApp.Business.DataTypes;
 using NSubstitute;
 using NUnit.Framework;
@@ -8,12 +9,12 @@ namespace DarvinAppTest.Business
     public class ExpertFixture
     {
         [Test]
-         public void ReadyToDecide_WithNoQuestionsAnswered_ExpectedFalse()
-         {
-             Expert expertToTest=new Expert();
-             bool actualReadyToDecide = expertToTest.ReadyToDecide();
-             Assert.False(actualReadyToDecide);
-         }
+        public void ReadyToDecide_WithNoQuestionsAnswered_ExpectedFalse()
+        {
+            var expertToTest = new Expert();
+            bool actualReadyToDecide = expertToTest.ReadyToDecide();
+            Assert.False(actualReadyToDecide);
+        }
 
         [TestCase(1)]
         [TestCase(10)]
@@ -21,9 +22,9 @@ namespace DarvinAppTest.Business
         [TestCase(100)]
         public void ReadyToDecide_WithAllQuestionsAnsweredNo_ExpectedFalse(int numberOfQuestions)
         {
-            Expert expertToTest = new Expert();
+            var expertToTest = new Expert();
 
-            for (int i = 0; i < numberOfQuestions;i++ )
+            for (int i = 0; i < numberOfQuestions; i++)
                 expertToTest.SubmitAnswer(Arg.Any<Question>(), false);
 
             bool actualReadyToDecide = expertToTest.ReadyToDecide();
@@ -34,9 +35,102 @@ namespace DarvinAppTest.Business
         [Test]
         public void Decision_NewlyCreatedObject_ThrownsNoExceptions()
         {
-            Expert expertToTest = new Expert();
-            Assert.DoesNotThrow(()=>expertToTest.Decision());
+            var expertToTest = new Expert();
+            Assert.DoesNotThrow(() => expertToTest.Decision());
         }
 
+        [Test]
+        public void Decision_ExpectedNotNullAnimalType()
+        {
+            var expertToTest = new Expert();
+            for (int i = 0; i < 5; i++)
+                expertToTest.SubmitAnswer(Arg.Any<Question>(), Arg.Any<bool>());
+            var decicion = expertToTest.Decision();
+            Assert.IsNotNull(decicion);
+            Assert.IsInstanceOf<AnimalType>(decicion);
+        }
+
+        [TestCase(AnimalType.FlowerVaseBreakers)]
+        [TestCase(AnimalType.Emperors)]
+        [TestCase(AnimalType.LookingLikeFlies)]
+        [TestCase(AnimalType.Piglets)]
+        [TestCase(AnimalType.RunningLikeCrazy)]
+        [TestCase(AnimalType.StrayDogs)]
+        [TestCase(AnimalType.Tale)]
+        [TestCase(AnimalType.Tamed)]
+        public void Decision_OneSingleTypedQuestionAnswer_ExpectedRighType(AnimalType type)
+        {
+            var expertToTest = new Expert();
+            var questionToAnswer = new Question(Arg.Any<string>())
+                {
+                    TypesGettingScoreFromPositiveAnswer = new List<AnimalType>
+                        {
+                            type
+                        }
+                };
+
+            expertToTest.SubmitAnswer(questionToAnswer, true);
+            Assert.AreEqual(expertToTest.Decision(), type);
+        }
+
+        [TestCase(AnimalType.FlowerVaseBreakers, AnimalType.Emperors)]
+        [TestCase(AnimalType.Emperors, AnimalType.FlowerVaseBreakers)]
+        [TestCase(AnimalType.LookingLikeFlies, AnimalType.Emperors)]
+        [TestCase(AnimalType.Piglets, AnimalType.FlowerVaseBreakers)]
+        [TestCase(AnimalType.RunningLikeCrazy, AnimalType.Emperors)]
+        [TestCase(AnimalType.StrayDogs, AnimalType.FlowerVaseBreakers)]
+        [TestCase(AnimalType.Tale, AnimalType.Emperors)]
+        [TestCase(AnimalType.Tamed, AnimalType.FlowerVaseBreakers)]
+        public void Decision_OneMultiTypedQuestionAnswer_ExpectedOther(AnimalType type1, AnimalType type2)
+        {
+            var expertToTest = new Expert();
+            var questionToAnswer = new Question(Arg.Any<string>())
+                {
+                    TypesGettingScoreFromPositiveAnswer = new List<AnimalType>
+                        {
+                            type1,
+                            type2
+                        }
+                };
+
+            expertToTest.SubmitAnswer(questionToAnswer, true);
+            Assert.AreEqual(AnimalType.Others, expertToTest.Decision());
+        }
+
+        [Test]
+        public void Decision_TwoExclusiveQuestions_ExpectedOthers()
+        {
+            var expertToTest = new Expert();
+            var type1 = Arg.Any<AnimalType>();
+            var type2 = Arg.Any<AnimalType>();
+
+            var questionToAnswer1 = new Question(Arg.Any<string>())
+                {
+                    TypesGettingScoreFromPositiveAnswer = new List<AnimalType>
+                        {
+                            type1,
+                        },
+                    TypesLosingScoreFromPositiveAnswer = new List<AnimalType>
+                        {
+                            type2
+                        }
+                };
+
+            var questionToAnswer2 = new Question(Arg.Any<string>())
+            {
+                TypesGettingScoreFromPositiveAnswer = new List<AnimalType>
+                        {
+                            type2,
+                        },
+                TypesLosingScoreFromPositiveAnswer = new List<AnimalType>
+                        {
+                            type1
+                        }
+            };
+
+            expertToTest.SubmitAnswer(questionToAnswer1,true);
+            expertToTest.SubmitAnswer(questionToAnswer2, true);
+            Assert.AreEqual(AnimalType.Others, expertToTest.Decision());
+        }
     }
 }
