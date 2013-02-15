609d435c4d527628a61624d1d11e9ce8d2a8b3bd
Nina Buchina <ninn-ok@mail.ru>
2013-02-15 13:49:53 +0400

    Юнит-тесты эксперта

---
+++ DarvinAppTest/Business/ExpertFixture.cs
@@ -0,0 +1,42 @@
+﻿using DarvinApp.Business;
+using DarvinApp.Business.DataTypes;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace DarvinAppTest.Business
+{
+    public class ExpertFixture
+    {
+        [Test]
+         public void ReadyToDecide_WithNoQuestionsAnswered_ExpectedFalse()
+         {
+             Expert expertToTest=new Expert();
+             bool actualReadyToDecide = expertToTest.ReadyToDecide();
+             Assert.False(actualReadyToDecide);
+         }
+
+        [TestCase(1)]
+        [TestCase(10)]
+        [TestCase(50)]
+        [TestCase(100)]
+        public void ReadyToDecide_WithAllQuestionsAnsweredNo_ExpectedFalse(int numberOfQuestions)
+        {
+            Expert expertToTest = new Expert();
+
+            for (int i = 0; i < numberOfQuestions;i++ )
+                expertToTest.SubmitAnswer(Arg.Any<Question>(), false);
+
+            bool actualReadyToDecide = expertToTest.ReadyToDecide();
+            Assert.False(actualReadyToDecide);
+        }
+
+
+        [Test]
+        public void Decision_NewlyCreatedObject_ThrownsNoExceptions()
+        {
+            Expert expertToTest = new Expert();
+            Assert.DoesNotThrow(()=>expertToTest.Decision());
+        }
+
+    }
+}
