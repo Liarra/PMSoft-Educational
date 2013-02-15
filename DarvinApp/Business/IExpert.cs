f96b21132613ceea93cef7e1d0c9f5d367e158b2
Nina Buchina <ninn-ok@mail.ru>
2013-02-15 13:49:27 +0400

    Изменён интерфейс IExpert
    Удалён сеттер свойства supportedMethods, добавлен метод для отправки
    ответа эксперту

--- DarvinApp/Business/IExpert.cs
+++ DarvinApp/Business/IExpert.cs
@@ -5,8 +5,10 @@ namespace DarvinApp.Business
 {
     public interface IExpert
     {
-        IList<AnimalType> SupportedTypes { get; set; }
+        IList<AnimalType> SupportedTypes { get; }
         bool ReadyToDecide();
         AnimalType Decision();
+
+        void SubmitAnswer(Question questionAnswered, bool answer);
     }
 }
