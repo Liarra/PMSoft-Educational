2078ef011a79bb4b8b49a649e7cc80bd4ea31d5b
Nina Buchina <ninn-ok@mail.ru>
2013-02-22 15:46:13 +0400

    Исправления по итогам CR -Animal
    Добавлены нормальные equals и hashcode

--- DarvinApp/Business/DataTypes/Animal.cs
+++ DarvinApp/Business/DataTypes/Animal.cs
@@ -1,21 +1,51 @@
-﻿namespace DarvinApp.Business.DataTypes
+﻿using System;
+
+namespace DarvinApp.Business.DataTypes
 {
     public class Animal
     {
-        public string Name { get; set; }
-        public AnimalType Type { get; set; }
+        private readonly string _name;
+        private readonly AnimalType _animalType;
+
+
+        public Animal(string name, AnimalType animalType)
+        {
+            if (name == null)
+                throw new ArgumentNullException("name");
+
+            _name = name;
+            _animalType = animalType;
+        }
+
+        public string Name
+        {
+            get { return _name; }
+        }
+
+        public AnimalType Type
+        {
+            get { return _animalType; }
+        }
 
         public override bool Equals(object o)
         {
-            if (o is Animal)
+            if (ReferenceEquals(null, o)) return false;
+            if (ReferenceEquals(this, o)) return true;
+            if (o.GetType() != GetType()) return false;
+            return Equals((Animal) o);
+        }
+
+        protected bool Equals(Animal other)
+        {
+            return string.Equals(_name, other._name) && _animalType == other._animalType;
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
             {
-                var anotherAnimal = o as Animal;
-                if (Name.Equals(anotherAnimal.Name)
-                    &&
-                    Type.Equals(anotherAnimal.Type))
-                    return true;
+                return (_name.GetHashCode() * 397) ^ ((int)(_animalType) + 1);
             }
-            return false;
         }
     }
 }
