c418a4f402da04a08f218c5c7eae85796ef4aee3
Nina Buchina <ninn-ok@mail.ru>
2013-03-06 18:55:47 +0400

    34543

--- DarvinApp/Business/Expert.cs
+++ DarvinApp/Business/Expert.cs
@@ -7,7 +7,7 @@ namespace DarvinApp.Business
 {
     public class Expert : IExpert
     {
-  ёёёёё
+  ёёёёё23
         private readonly IList<AnimalType> _supportedTypes;
         private readonly Dictionary<AnimalType, int> _scoretable;
 
