987e184921fe00d9009145afad8f878e92546ff2
Nina Buchina <ninn-ok@mail.ru>
2013-03-06 18:49:51 +0400

    Ошиба :)

--- DarvinApp/Business/Expert.cs
+++ DarvinApp/Business/Expert.cs
@@ -7,7 +7,7 @@ namespace DarvinApp.Business
 {
     public class Expert : IExpert
     {
-        private String oooo = "o";
+  ёёёёё
         private readonly IList<AnimalType> _supportedTypes;
         private readonly Dictionary<AnimalType, int> _scoretable;
 
